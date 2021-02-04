5530bbfaf587f74fa748b0278175a1f24bafaa3a
ignacio <ignaciojaviercaro@gmail.com>
2021-02-04 16:35:54 -0300

    primr

--- app/src/main/java/com/example/tareakotlinisbn/LibrosEscolares.kt
+++ app/src/main/java/com/example/tareakotlinisbn/LibrosEscolares.kt
@@ -1,8 +1,44 @@
 package com.example.tareakotlinisbn
 
+import kotlin.math.E
+
 class LibrosEscolares {
 
-    fun precioFormateado(any: Any){
-        
-    }
+
+}
+fun main(){
+
+val bookList = mutableListOf("Tom Sawyer", 123456789, 1843, "4343", "Mark Twain", 'I' )
+
+
+
+
+    bookList.add ("Hijo de ladrón",12349087654, 1957, "$4356", "Manuel Rojas", 'I')
+
+
+
+println(bookList)
+
+
+
+
+
+
+}
+
+
+//fun <agregar> MutableList<agregar>.add(a: )
+
+ fun <agregar> MutableList<agregar>.add(Nombre: String, ISBN: Long, Año: Int, Precio: String, Autor: String, Formato: Char) {
+
+}
+
+fun bookList(Nombre:String, ISBN:Long, Año:Int, Precio:String, Autor:String, Formato:Char ){
+
+}
+fun addLibros(any: Any){
+    bookList("Hijo de ladrón",12349087654, 1957, "$4356", "Manuel Rojas", 'I')
+}
+fun buscadorPrecio(any: Any){
+
 }
